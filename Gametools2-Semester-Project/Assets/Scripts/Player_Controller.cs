eab3cb2ff93d4eb06f16d1279c296b0e12a31730
kb478 <c23789389@mytudublin.ie>
2024-10-15 15:39:45 +0100

    Gun swapping + plasma gun knockback
    Need to code in damaging enemies

--- Gametools2-Semester-Project/Assets/Scripts/Player_Controller.cs
+++ Gametools2-Semester-Project/Assets/Scripts/Player_Controller.cs
@@ -14,7 +14,24 @@ public class Player_Controller : MonoBehaviour
 
     // Update is called once per frame
 
+    private void OnPrevious()
+    {
+        gun_Control_Script.current_Weapon_I--;
+        if (gun_Control_Script.current_Weapon_I < 0)
+            gun_Control_Script.current_Weapon_I = gun_Control_Script.total_Weapons;
+        
+        gun_Control_Script.Change_Weapon();
+    }
 
+    private void OnNext()
+    {
+        gun_Control_Script.current_Weapon_I++;
+        if (gun_Control_Script.current_Weapon_I > gun_Control_Script.total_Weapons)
+            gun_Control_Script.current_Weapon_I = 0;
+        
+        gun_Control_Script.Change_Weapon();
+    }
+    
     private void OnAttack()
     {
         gun_Control_Script.Shoot();
