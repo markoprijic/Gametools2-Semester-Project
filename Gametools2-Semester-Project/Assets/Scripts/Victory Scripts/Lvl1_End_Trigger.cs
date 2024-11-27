5e835f7f49bbcad54e48a91caaf2f278f42cddb0
kb478 <c23789389@mytudublin.ie>
2024-11-27 11:40:58 +0000

    Revert "Merge branch 'New-Prefabs' into Development"
    This reverts commit 13264a2f31c1336067197e312b187950c1af72e6, reversing
    changes made to dddcab57ad87c8a570efede65ed362b46d7fadb5.

--- Gametools2-Semester-Project/Assets/Scripts/Victory Scripts/Lvl1_End_Trigger.cs
+++ Gametools2-Semester-Project/Assets/Scripts/Victory Scripts/Lvl1_End_Trigger.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class Lvl1_End_Trigger : MonoBehaviour
 {
-    [SerializeField] private Victory_Manager victory_Script;
+    [SerializeField] private Lvl1_Victory victory_Script;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            victory_Script.Next_Scene();
+            victory_Script.Trigger_End();
         }
     }
 }
